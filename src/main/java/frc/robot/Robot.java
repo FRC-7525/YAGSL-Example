e3d15bc13e067f6fe505f0edeb7e32d96b76220f
Liquid2112 <ShubhPatel2504@gmail.com>
2023-09-13 17:20:17 -0500

    changed encoder values

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -13,6 +13,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import swervelib.SwerveDrive;
 import swervelib.parser.*;
+import swervelib.SwerveController;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -41,6 +42,8 @@ public class Robot extends TimedRobot {
     } catch (IOException e) {
       e.printStackTrace();
     }
+
+    SwerveController
   }
 
   /**
